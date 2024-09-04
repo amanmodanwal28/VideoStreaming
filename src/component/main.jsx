3663afff400b7443dcb0013d3d5409e1207eedaf
anshi1208 <ankitasinghrajput5008@gmail.com>
2024-09-05 00:16:16 +0530

    Here is the fix of main UI

--- src/component/main.jsx
+++ src/component/main.jsx
@@ -1,30 +1,29 @@
-
-import { createRoot } from 'react-dom/client'
-
-import Search from './Search'
-import LoginIdBar from './Login'
-import '../css/index.scss'
+import '../css/index.scss';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Sidebar from "../component/Sidebar";
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-
 import Search from './Search';
 import LoginIdBar from './Login';
-import '../css/index.scss';
 import MainContent from './MainContent';
 import Footer from './Footer';
 
 createRoot(document.getElementById('root')).render(
   <div className="app-container">
     <header className="header">
-     <Sidebar/>
-      <Search />
-      <LoginIdBar />
+      <div className="sidebar-container">
+        <Sidebar />
+      </div>
+      <div className="search-login-container">
+        <Search />
+        <LoginIdBar />
+      </div>
     </header>
     <main>
-      <MainContent />
+      <div className="main-content-container">
+        <MainContent />
+      </div>
     </main>
     <Footer />
   </div>
