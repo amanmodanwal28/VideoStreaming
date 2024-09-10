9e515435c5b64769e3b27d431b0e9af78679cafb
Aman Modanwal <itisaman28@gmail.com>
2024-09-10 15:00:15 +0530

    merge

--- src/component/main.jsx
+++ src/component/main.jsx
@@ -2,19 +2,27 @@ import Search from './Search'
 import LoginIdBar from './Login'
 import '../css/index.scss'
 import "bootstrap/dist/css/bootstrap.min.css";
-import Sidebar from "../component/Sidebar";
+// import Sidebar from "../component/Sidebar";
 import '../css/index.scss';
 import { createRoot } from 'react-dom/client';
 import MainContent from './MainContent';
 import Footer from './Footer';
+import Hamburger from './Hamburger'
+import { RxHamburgerMenu } from 'react-icons/rx'
+
+
 
 createRoot(document.getElementById('root')).render(
   <div className="app-container">
     <header className="header">
-      <div className="sidebar-container">
-        <Sidebar />
-      </div>
+      {/* <div className="sidebar-container"><Sidebar /></div> */}
       <div className="search-login-container">
+        <div>
+          <span style={{ padding: '1rem .5rem' }}>
+            <RxHamburgerMenu size={30} color="black" />
+          </span>
+        </div>
+        <Hamburger />
         <Search />
         <LoginIdBar />
       </div>
